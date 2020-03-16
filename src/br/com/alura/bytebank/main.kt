ded4d62caf5e09f34d94ae5be653c14d79dab35e
Alex Felipe <alexfelipevieira@gmail.com>
2020-03-16 15:17:44 -0300

    amostra de pilha

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,7 +1,21 @@
 package br.com.alura.bytebank
 
-import br.com.alura.bytebank.teste.testaAny
-
 fun main() {
-    testaAny()
+    println("início main")
+    funcao1()
+    println("fim main")
+}
+
+fun funcao1(){
+    println("início funcao1")
+    funcao2()
+    println("fim funcao1")
+}
+
+fun funcao2() {
+    println("início funcao2")
+    for (i in 1..5){
+        println(i)
+    }
+    println("fim funcao2")
 }
