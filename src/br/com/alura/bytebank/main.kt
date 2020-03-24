f3ddc8316a17cf27d3234cf919dad6b3040b415c
Alex Felipe <alexfelipevieira@gmail.com>
2020-03-24 08:10:14 -0300

    2.1

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,23 +1,27 @@
 package br.com.alura.bytebank
 
 import br.com.alura.bytebank.modelo.Endereco
+import java.lang.ClassCastException
 
 fun main() {
     println("início main")
-    10/0
     funcao1()
     println("fim main")
 }
 
-fun funcao1(){
+fun funcao1() {
     println("início funcao1")
-    funcao2()
+    try {
+        funcao2()
+    } catch (e: ClassCastException) {
+        println("ClassCastException foi pegada")
+    }
     println("fim funcao1")
 }
 
 fun funcao2() {
     println("início funcao2")
-    for (i in 1..5){
+    for (i in 1..5) {
         println(i)
         val endereco = Any()
         endereco as Endereco
