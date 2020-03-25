934d4c43eb515f655d1ec84164131dafa2f531a4
Alex Felipe <alexfelipevieira@gmail.com>
2020-03-25 08:41:29 -0300

    3.1

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,34 +1,7 @@
 package br.com.alura.bytebank
 
-import br.com.alura.bytebank.modelo.Endereco
-import java.lang.ClassCastException
-import java.lang.NumberFormatException
-
 fun main() {
     println("início main")
-    val entrada: String = "1.0"
-
-    val valorRecebido: Double? = try {
-        entrada.toDouble()
-    } catch (e: NumberFormatException) {
-        println("Problema na conversão")
-        e.printStackTrace()
-        null
-    }
-
-    val valorComTaxa: Double? = if (valorRecebido != null) {
-        valorRecebido + 0.1
-    } else {
-        null
-    }
-
-    if (valorComTaxa != null) {
-        println("valor recebido: $valorComTaxa")
-    } else {
-        println("valor inválido")
-    }
-
-
     funcao1()
     println("fim main")
 }
@@ -49,7 +22,7 @@ fun funcao2() {
     for (i in 1..5) {
         println(i)
         val endereco = Any()
-        endereco as Endereco
+        throw ArithmeticException()
     }
     println("fim funcao2")
 }
